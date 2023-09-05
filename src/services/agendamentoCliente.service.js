90111e6ae57ddc182995df3449cdfd49623e590b
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-05 11:13:55 -0300

    Excluindo agendamento pelo id

--- src/services/agendamentoCliente.service.js
+++ src/services/agendamentoCliente.service.js
@@ -1,8 +1,12 @@
 import agendamentosClentes from "../models/agendamentosClientes.js";
 
-const createService = (body) => agendamentosClentes.create(body); //Agenda.create método proprio do mongosse.
+const createService = (body) => agendamentosClentes.create(body); 
 
-const findAllService = () => agendamentosClentes.find().populate("servico").populate("user");//Agenda.find método proprio do mongosse.
+const findAllService = () => agendamentosClentes.find().populate("servico").populate("user");
+
+const findByIdServiceAgendamento = (id) => agendamentosClentes.findById(id).populate("user");
+
+const eraseService = (id) => agendamentosClentes.findOneAndDelete({_id: id});
 
 //Exportando create
-export { createService, findAllService };
+export { createService, findAllService,findByIdServiceAgendamento, eraseService };
