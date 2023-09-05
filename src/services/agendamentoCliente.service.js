8c429282289b9e594fc6e4f448933ff8c150b186
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-05 09:14:48 -0300

    Atualizando controle de agendamentos, adicionando mais uma verificação para criação

--- src/services/agendamentoCliente.service.js
+++ src/services/agendamentoCliente.service.js
@@ -2,7 +2,7 @@ import agendamentosClentes from "../models/agendamentosClientes.js";
 
 const createService = (body) => agendamentosClentes.create(body); //Agenda.create método proprio do mongosse.
 
-const findAllService = () => agendamentosClentes.find().populate("servico").populate("user").populate("userADM");//Agenda.find método proprio do mongosse.
+const findAllService = () => agendamentosClentes.find().populate("servico").populate("user");//Agenda.find método proprio do mongosse.
 
 //Exportando create
 export { createService, findAllService };
