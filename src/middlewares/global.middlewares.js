65d7a8480ec9aa8b3b0abd0c849eab1a5826fa0c
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-25 22:09:21 -0300

    Update global.middlewares.js

--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import userService from "../services/user.service.js";
 import { findByIdService } from "../services/servicos.service.js";
-//Função para validar o id Fornecido.
+
 const validId = (req, res, next) => {
   try {
     const id = req.params.id;
@@ -16,30 +16,25 @@ const validId = (req, res, next) => {
   }
 };
 
-//Função para validar dados do usuario.
 const validUser = async (req, res, next) => {
   try {
     const id = req.params.id;
-    const user = await userService.findByIdService(id); //Buscando no banco de dados.
+    const user = await userService.findByIdService(id); 
     if (!user) {
       return res.status(400).send({ message: "Usuario não encontrado" });
     }
-
-    //Enviando id e user para a proxima função que está em user.service.js
     req.id = id;
     req.user = user;
-    next(); //Função para deixar prosseguir.
+    next();
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
 };
 
-//Função para validar o id Fornecido.
 const validIdServico = (req, res, next) => {
   try {
     const id = req.params.id;
 
-    //Verificando se o ID é valido, com propriedade do mongoose
     if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(400).send({ message: "Id invalido" });
     }
@@ -49,15 +44,13 @@ const validIdServico = (req, res, next) => {
   }
 };
 
-//Função para validar dados do serviço.
 const validServico = async (req, res, next) => {
   try {
     const id = req.params.id;
-    const servico = await findByIdService(id); //Buscando no banco de dados.
+    const servico = await findByIdService(id);
     if (!servico) {
       return res.status(400).send({ message: "Serviço não encontrado" });
     }
-    //Enviando id e user para a proxima função que está em user.service.js
     req.id = id;
     req.servico = servico;
     next(); //Função para deixar prosseguir.
@@ -66,11 +59,9 @@ const validServico = async (req, res, next) => {
   }
 };
 
-//Função para validar o id Fornecido.
 const validIdAgendamento = (req, res, next) => {
   try {
     const { servico } = req.body;
-    //Verificando se o ID é valido, com propriedade do mongoose
     if (!mongoose.Types.ObjectId.isValid(servico)) {
       return res.status(400).send({ message: "Id invalido" });
     }
@@ -80,17 +71,15 @@ const validIdAgendamento = (req, res, next) => {
   }
 };
 
-//Função para validar dados do serviço.
 const validAgendamento = async (req, res, next) => {
   try {
     const { servico } = req.body;
-    const servicoExistente = await findByIdService(servico); //Buscando no banco de dados.
+    const servicoExistente = await findByIdService(servico);
     if (!servicoExistente) {
       return res.status(400).send({ message: "Serviço não encontrado" });
     }
-    //Enviando id e user para a proxima função que está em user.service.js
     req.servico = servico;
-    next(); //Função para deixar prosseguir.
+    next();
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
