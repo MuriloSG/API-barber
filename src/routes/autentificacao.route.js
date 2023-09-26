6097729ce3e7e43fb5327b0945b01a9d4c4d320d
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-25 22:13:27 -0300

    Update autentificacao.route.js

--- src/routes/autentificacao.route.js
+++ src/routes/autentificacao.route.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
-const router = Router();
-
 import { login, loginADM } from "../controllers/autentificacao.controller.js";
 
-//Rota para logar um usuario.
+const router = Router();
+
 router.post("/", login);
 
-//Rota para logar um usuario administrador.
 router.post("/adm", loginADM);
 
 export default router;
