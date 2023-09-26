c66f54870d515f6ee930c58de2c68858423fcda4
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-25 22:16:27 -0300

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,10 @@
 import User from "../models/User.js";
 
-const createService = (body) => User.create(body); //User.create método proprio do mongosse.
+const createService = (body) => User.create(body);
 
-const findAllService = () => User.find(); //User.find método proprio do mongosse.
+const findAllService = () => User.find();
 
-const findByIdService = (id) => User.findById(id); //User.findById método proprio do mongosse.
+const findByIdService = (id) => User.findById(id);
 
 const updateService = (id, nome, email, Nomeusuario, telefone, avatar) =>
   User.findByIdAndUpdate(
@@ -16,9 +16,8 @@ const updateService = (id, nome, email, Nomeusuario, telefone, avatar) =>
       telefone,
       avatar,
     }
-  ); //User.findByIdAndUpdate método proprio do mongosse, atualiza pelo id os campos que recebeu, se rercebeu apenas um atributo o patch atualiza ele apenas.
+  );
 
-//Exportando create
 export default {
   createService,
   findAllService,
