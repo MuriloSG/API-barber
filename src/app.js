a6c34f5b484fe67350761714ba3488fadff3f81c
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-25 22:17:43 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,32 +1,22 @@
 import express from "express";
-
-//importando rotas.
 import userRoute from "./routes/user.route.js";
 import autentificacaoRoute from "./routes/autentificacao.route.js";
 import userADMRoute from "./routes/userADM.route.js";
 import servicosRoute from "./routes/servicos.route.js";
 import agendamentosClientes from "./routes/agendamentosClientes.route.js";
 
-//Criando uma instancia para o express.
 const app = express();
 
-//Mostrar para o express ler body com json.
 app.use(express.json());
 
-//Usando rota de usuarios
 app.use("/usuarios", userRoute);
 
-//Usando rota de useuarios administradores.
 app.use("/usuariosADM", userADMRoute);
 
-//Usando rota de login usuario.
 app.use("/autentificacao", autentificacaoRoute);
 
-//Usando rota de Serviços.
 app.use("/servicos", servicosRoute);
 
-//Usando rota de Agendamento dos clientes.
 app.use("/agendamento", agendamentosClientes);
 
-//Exportando para sunir o servirdo local.
 export default app;
