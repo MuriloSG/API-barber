1a7f2ddfce34251fb3dd9dc74882b02d39e50aeb
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-25 22:11:40 -0300

    Update UserADM.js

--- src/models/UserADM.js
+++ src/models/UserADM.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
-
-//Importando bcrypt, comando no terminal npm i brcypt.
 import bcrypt from "bcrypt"
 
-//Criando schema Usuarios Administradores.
 const UserADMSchema = new mongoose.Schema({
   nome: {
     type: String,
@@ -30,14 +27,11 @@ const UserADMSchema = new mongoose.Schema({
   },
 });
 
-//Funçào para fazer alteraçoes antes de enviar para o banco de dados.
 UserADMSchema.pre("save", async function (next) {
-  //Criptografando a senha do usuario.
   this.senha = await bcrypt.hash(this.senha, 10);
   next(); //Função para seguir em frente.
 });
 
-//Definindo Schema Usuario administrador.
 const UserADM = mongoose.model("UserADM", UserADMSchema);
 
 export default UserADM;
