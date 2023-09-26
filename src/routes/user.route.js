a7630f967a5afd046c0b416c6b4518562572a419
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-25 22:14:31 -0300

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,18 +1,13 @@
 import { Router } from "express";
 import userControllers from "../controllers/user.controller.js";
-
-//Importando middlewares(interceptador).
 import globalMiddlewares from "../middlewares/global.middlewares.js";
 
 const route = Router();
 
-//Inserindo Usuario.
 route.post("/", userControllers.create);
 
-//Pegando Usuario
 route.get("/", userControllers.findAll);
 
-//Pegando Usuario por id
 route.get(
   "/:id",
   globalMiddlewares.validId,
@@ -20,7 +15,6 @@ route.get(
   userControllers.findById
 );
 
-//Atualizando usuario.
 route.patch(
   "/:id",
   globalMiddlewares.validId,
@@ -28,7 +22,4 @@ route.patch(
   userControllers.update
 );
 
-
-
-//exportando rotas.
 export default route;
