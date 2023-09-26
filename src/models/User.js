d5dd677b51270e7d499ffccdf4cfbb1afc96dc29
MuriloSG <115160124+MuriloSG@users.noreply.github.com>
2023-09-25 22:10:45 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
-
-//Importando bcrypt, comando no terminal npm i brcypt.
 import bcrypt from "bcrypt"
 
-//Criando schema Usuarios.
 const UserSchema = new mongoose.Schema({
   nome: {
     type: String,
@@ -34,14 +31,12 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-//Funçào para fazer alteraçoes antes de enviar para o banco de dados.
 UserSchema.pre("save", async function (next) {
   //Criptografando a senha do usuario.
   this.senha = await bcrypt.hash(this.senha, 10);
   next(); //Função para seguir em frente.
 });
 
-//Definindo Schema.
 const User = mongoose.model("User", UserSchema);
 
 export default User;
